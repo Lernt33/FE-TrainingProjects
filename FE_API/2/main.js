0f5c3b8facbea2a9ac4eadfb46f8a95fde2b97ba
Lernt (Georgii Zautashvili) <145955062+Lernt33@users.noreply.github.com>
2024-04-02 21:33:01 +0400

    Update main.js

--- FE_API/2/main.js
+++ FE_API/2/main.js
@@ -2,29 +2,34 @@ const config = {
   brands: [],
   ALL: [],
   PrevSelectedBrand: "ALL",
+  page: 1,
+  maxpage: 8,
 };
 
 const Table = document.getElementById("main-table");
 const Brands = document.querySelector(".form-select");
 const selector = document.getElementById("ALL");
+const buttons = document.querySelectorAll(".btn");
 // const PriceRegulator = document.querySelectorAll(".PriceRegulator");
 // LOADING DATA FROM API
-Init();
-//
+LoadProducts();
 
-function Init() {
+async function LoadProducts(page = 1) {
   const xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://api.everrest.educata.dev/shop/products/all");
+  xhr.open(
+    "GET",
+    `https://api.everrest.educata.dev/shop/products/all?page_index=${page}`,
+  );
   xhr.send();
   xhr.onload = () => {
     const products = JSON.parse(xhr.responseText).products;
     products.forEach((el) => {
-      if (!config.brands.includes(el.brand)) {
-        config.brands.push(el.brand);
+      if (!config.brands.includes(el.brand.toUpperCase())) {
+        config.brands.push(el.brand.toUpperCase());
       }
       const iterHTML = `    <tr class="product">
         <td  class="fs-5 text-wrap title" style="width: 10rem;">${el.title}</td>
-        <td class="fs-4 brand">${el.brand}</td>
+        <td class="fs-4 brand">${el.brand.toUpperCase()}</td>
         <td class="fs-6 text-wrap desc" >${el.description} </td>
         <td class="fs-2 font-weight-bold price" >${el.price.current}$</td>
         <td class="img"><img width="200px" height="200px" src="${el.images[0]}" class="user-select-none img-thumbnail img-fluid" alt="...">
@@ -45,6 +50,15 @@ function Init() {
   };
 }
 
+function sort(brand) {
+  Table.innerHTML = "";
+  config.ALL.forEach((el) => {
+    if (el.includes(`<td class="fs-4 brand">${brand}</td>`)) {
+      Table.innerHTML += el;
+    }
+  });
+}
+
 // SELECTOR
 selector.addEventListener("click", function () {
   if (selector.value === config.PrevSelectedBrand) {
@@ -61,30 +75,23 @@ selector.addEventListener("click", function () {
   sort(selector.value);
 });
 //
-function sort(brand) {
-  Table.innerHTML = "";
-  config.ALL.forEach((el) => {
-    if (el.includes(`<td class="fs-4 brand">${brand}</td>`)) {
-      Table.innerHTML += el;
+
+buttons.forEach((el) => {
+  el.addEventListener("click", () => {
+    if (el.textContent == ">") {
+      config.page += 1 ? config.page < config.maxpage && config.page >= 1 : {};
+      Loadnext();
+    } else {
+      config.page -= 1 ? config.page <= config.maxpage && config.page > 1 : {};
+      Loadnext();
     }
   });
+});
+
+function Loadnext() {
+  config.brands = [];
+  config.ALL = [];
+  Brands.innerHTML = '<option class="selector">ALL</option>';
+  Table.innerHTML = "";
+  LoadProducts((page = config.page));
 }
-//
-// PriceRegulator.forEach((el) =>
-//   el.addEventListener("keyup", (event) => {
-//     if (event.key === "Enter") {
-//       SortByPrice();
-//     }
-//   }),
-// );
-// function SortByPrice() {
-//   const min = document.getElementById("MinPrice");
-//   const max = document.getElementById("MaxPrice");
-//   const trs = document.querySelectorAll(".product");
-//   trs.forEach((el) => {
-//     const price = el.querySelector(".price").text.slice(0, -1);
-//     if (min <= price && price >= max) {
-//       Table.innerHTML += el;
-//     }
-//   });
-// }
